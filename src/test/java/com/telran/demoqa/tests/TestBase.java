7e94193c29966f06940bad8421ab0df266e66308
Kate <levifin7@gmail.com>
2021-12-05 23:11:10 +0300

    added Listener

--- src/test/java/com/telran/demoqa/tests/TestBase.java
+++ src/test/java/com/telran/demoqa/tests/TestBase.java
@@ -1,8 +1,12 @@
 package com.telran.demoqa.tests;
 
 import com.telran.demoqa.pages.PageBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
@@ -14,17 +18,40 @@ import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
-	public WebDriver driver;
+//		public WebDriver driver;
+	public EventFiringWebDriver driver;
 
 	Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+	public static class MyListener extends AbstractWebDriverEventListener {
+
+		Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+		@Override
+		public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+			logger.info("Start search " + by);
+		}
+
+		@Override
+		public void afterFindBy(By by, WebElement element, WebDriver driver) {
+			logger.info(by + " found.");
+		}
+
+		@Override
+		public void onException(Throwable throwable, WebDriver driver) {
+			logger.error(throwable.toString());
+		}
+	}
 
 	@BeforeMethod
 	public void setUp(Method m) {
 		logger.info("Start test " + m.getName());
-		driver = new ChromeDriver();
+		driver = new EventFiringWebDriver(new ChromeDriver());
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://demoqa.com");
+
+		driver.register(new MyListener());
 	}
 
 	@AfterMethod

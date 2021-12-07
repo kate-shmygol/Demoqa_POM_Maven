4dd2602cad06e7b9659f184ef24624915b4e704a
Kate <levifin7@gmail.com>
2021-12-07 20:40:03 +0300

    added correct DataProvider with CSV

--- src/test/java/com/telran/demoqa/data/DataProviders.java
+++ src/test/java/com/telran/demoqa/data/DataProviders.java
@@ -2,10 +2,7 @@ package com.telran.demoqa.data;
 
 import org.testng.annotations.DataProvider;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -13,57 +10,16 @@ import java.util.List;
 public class DataProviders {
 
 	@DataProvider
-	public Iterator<Object[]> newStudent() {
-
-		List<Object[]> list = new ArrayList<>();
-		list.add(new Object[]{
-				StudentData.FIRST_NAME,
-				StudentData.LAST_NAME,
-				StudentData.E_MAIL,
-				StudentData.PHONE,
-				StudentData.GENDER,
-				StudentData.B_DAY,
-				StudentData.SUBJECTS,
-				StudentData.HOBBIES,
-				StudentData.FILE
-//				StudentData.ADDRESS
-//				StudentData.STATE
-//				StudentData.CITY
-		});
-
-		return list.iterator();
-	}
-
-	@DataProvider
-	public Iterator<Object[]> newStudentFromCSV() throws IOException {
-
-		List<Object[]> list = new ArrayList<>();
-		BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/Stundents.csv")));
-
-		String line = reader.readLine();
-
+	public static Iterator<Object[]> newStudentFromCSV() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				DataProviders.class.getResourceAsStream("/Stundents.csv")));
+		List<Object[]> userData = new ArrayList<Object[]>();
+		String line = in.readLine();
 		while (line != null) {
-
-			String[] split = line.split(",");
-
-			list.add(new Object[]{
-//					StudentData.FIRST_NAME(split[0]),
-					StudentData.LAST_NAME,
-					StudentData.E_MAIL,
-					StudentData.PHONE,
-					StudentData.GENDER,
-					StudentData.B_DAY,
-					StudentData.SUBJECTS,
-					StudentData.HOBBIES,
-					StudentData.FILE
-//				StudentData.ADDRESS
-//				StudentData.STATE
-//				StudentData.CITY});
-			});
-
-			line = reader.readLine();
+			userData.add(line.split(","));
+			line = in.readLine();
 		}
-
-		return list.iterator();
+		in.close();
+		return userData.iterator();
 	}
 }
